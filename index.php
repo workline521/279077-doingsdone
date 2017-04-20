69cc3ba39eec80d6cb8b05770d7ddbccb8d55af7
Evgenii Isupov <workline521@gmail.com>
2017-04-20 20:33:32 +0400

    Working with time variables

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 $days = rand(0, 3);
 $task_deadline_ts = strtotime("+" . $days . " day"); // метка времени даты выполнения задачи
 $current_ts = time(); // текущая метка времени
-
+$current_day = date("d.m.Y", $current_ts);
 // запишите сюда дату выполнения задачи в формате дд.мм.гггг
-$date_deadline = null;
+$date_deadline = date("d.m.Y", $task_deadline_ts);
 
 // в эту переменную запишите кол-во дней до даты задачи
-$days_until_deadline = null;
+$days_until_deadline = $date_deadline - $current_day ;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -135,7 +135,10 @@ $days_until_deadline = null;
                     </tr>
 
                     <!--добавьте здесь класс "task--important" если эта задача просрочена-->
-                    <tr class="tasks__item task">
+                    <tr  class="tasks__item task
+                    <?= ($days_until_deadline <= 0) ?  'task--important' : '' ?>
+
+                    ">
                         <td class="task__select">
                             <label class="checkbox task__checkbox">
                                 <input class="checkbox__input visually-hidden" type="checkbox">
@@ -144,6 +147,7 @@ $days_until_deadline = null;
                         </td>
 
                         <td class="task__date">
+                        <?= $date_deadline ?>
                             <!--выведите здесь дату выполнения задачи-->
                         </td>
 
